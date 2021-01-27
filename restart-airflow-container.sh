69a9b433a16ceb028f0389370767d88e0a3c68b9
unknown <tremper@teco.edu>
2021-01-27 15:20:29 +0100

    added cronjob monitor script for container; changed config to wait 120 sec for python scripts in creating dags

---
+++ restart-airflow-container.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# adapted from https://www.codegrepper.com/code-examples/typescript/how+to+get+docker+stats+using+shell+script
+
+# for logging purposes
+echo "--- --- ---"
+date
+docker container ls -a
+docker stats --no-stream
+
+# check if container is down, else check memory usage
+if [ ! "$(docker ps -q -f name=agitated_raman)" ]
+then 
+    echo "Container was down. Restarting. "
+    docker restart agitated_raman
+else
+    # script to restart container if memory usage is too high
+    IFS=;
+    DOCKER_STATS_CMD=`docker stats --no-stream --format "table {{.MemPerc}}\t{{.CPUPerc}}\t{{.MemUsage}}\t{{.Name}}"`
+    SUM_RAM=`echo $DOCKER_STATS_CMD | tail -n +2 | sed "s/%//g" | awk '{s+=$1} END {print s}'`
+
+    SUM_RAM_INT=${SUM_RAM%.*}
+
+    RAM_THRESHOLD=70
+
+    if [ "${SUM_RAM_INT}" -ge "${RAM_THRESHOLD}" ]
+        then echo "Memory usage ${SUM_RAM_INT}% is larger than the threshold of ${RAM_THRESHOLD}%"
+            echo "restarting Container"
+            docker restart agitated_raman
+        else echo "Memory usage ${SUM_RAM_INT}% is less than the threshold of ${RAM_THRESHOLD}%"
+            echo "skipping restart"
+    fi
+fi
